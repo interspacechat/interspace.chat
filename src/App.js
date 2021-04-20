57d8f9604b09a650246494b20aae910816c1faf8
geleeroyale <krassvs@gmail.com>
2021-04-20 16:23:22 +0200

    sovrython init

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
-import React from "react";
-import styled from "@emotion/styled";
-import { BrowserView } from "react-device-detect";
+import React from 'react'
+import styled from '@emotion/styled'
+import { BrowserView } from 'react-device-detect'
 
-import Space from "./components/Space";
-import FloatingRoomWindow from "./components/FloatingRoomWindow";
-import RootContextProvider from "./contexts/RootContext";
-import SpaceContext from "./contexts/SpaceContext";
+import Space from './components/Space'
+import FloatingRoomWindow from './components/FloatingRoomWindow'
+import RootContextProvider from './contexts/RootContext'
+import SpaceContext from './contexts/SpaceContext'
 
-import Header from "./components/Header";
+import Header from './components/Header'
+import Announcement from './components/Announcement'
 
-const Wrapper = styled("div")`
-  color: ${(props) => props.theme.body};
-  background-color: ${(props) => props.theme.background};
+const Wrapper = styled('div')`
+  color: ${props => props.theme.body};
+  background-color: ${props => props.theme.background};
   max-width: 100vw;
   min-height: 100%;
 
   a {
-    color: ${(props) => props.theme.highlight};
+    color: ${props => props.theme.highlight};
   }
-`;
+`
 
-function App() {
+function App () {
   return (
-    <Wrapper className="App">
+    <Wrapper className='App'>
       <RootContextProvider>
         <SpaceContext>
-          <Space />
+          <Announcement />
         </SpaceContext>
         <FloatingRoomWindow />
       </RootContextProvider>
     </Wrapper>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Announcement.js
@@ -0,0 +1,76 @@
+import React, { Fragment, useContext, useState } from 'react'
+import styled from 'styled-components'
+import { useMediaQuery } from 'react-responsive'
+import { FloatingSpaceContext } from '../contexts/FloatingSpaceContext'
+
+import AnnouncementImage from '../img/sovrython/announcement.png'
+
+const StyledLink = styled.a`
+  color: #e340a8;
+  text-align: center;
+`
+const ImageButton = styled.button`
+  cursor: pointer;
+  transform: scale(1);
+  transition: 1s;
+
+  & :hover {
+    transform: scale(2);
+  }
+`
+
+const Announce = () => {
+  const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
+  const isMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+  const {
+    currentFloatingSpaces,
+    addFloatingSpace,
+    closeFloatingSpace
+  } = useContext(FloatingSpaceContext)
+
+  let displayedJoinedSpaces
+  if (currentFloatingSpaces.length > 0) {
+    if (currentFloatingSpaces.length > 2) {
+      let nameCount = currentFloatingSpaces.length
+      displayedJoinedSpaces =
+        currentFloatingSpaces.slice(0, nameCount - 2).join(', ') +
+        ', ' +
+        currentFloatingSpaces.slice(nameCount - 2, nameCount).join(' & ')
+    } else {
+      displayedJoinedSpaces = currentFloatingSpaces.join(' & ')
+    }
+  }
+
+  return (
+    <React.Fragment>
+      {isPortrait && isMobile ? (
+        <div
+          style={{
+            width: '100vw',
+            height: '100vh',
+            display: 'grid',
+            placeContent: 'center'
+          }}
+        >
+          <img src={AnnouncementImage} alt='' width='100%' height='auto' />
+        </div>
+      ) : (
+        <React.Fragment>
+          <div
+            style={{
+              width: '100vw',
+              position: 'static',
+              height: '100vh',
+              display: 'grid',
+              placeItems: 'center'
+            }}
+          >
+            <img src={AnnouncementImage} alt='' width='100%' height='auto' />
+          </div>
+        </React.Fragment>
+      )}
+    </React.Fragment>
+  )
+}
+
+export default Announce
