0ca90951c104380c41d30fde874cc61089c67777
geleeroyale <krassvs@gmail.com>
2021-06-03 13:50:50 +0200

    add powered by logos

--- src/components/Space.js
+++ src/components/Space.js
@@ -1,33 +1,55 @@
-import React, { Fragment, useContext, useState, useEffect } from "react";
-import styled from "@emotion/styled";
-import { BrowserView, MobileView } from "react-device-detect";
+import React, { Fragment, useContext, useState, useEffect } from 'react'
+import styled from '@emotion/styled'
+import { BrowserView, MobileView } from 'react-device-detect'
 
-import { FloatingSpaceContext } from "../contexts/FloatingSpaceContext";
+import { FloatingSpaceContext } from '../contexts/FloatingSpaceContext'
 
-import SvgImagemap from "./Sovrython";
-import SvgImagemapMobile from "./SovrythonMobile";
+import SvgImagemap from './Sovrython'
+import SvgImagemapMobile from './SovrythonMobile'
 
-import triangle from "../img/triangle.svg";
+import ParalleleLogo from '../img/sovrython/Parallele-logo.svg'
+import GitcoinLogo from '../img/sovrython/Gitcoin-logo.svg'
+import InterspaceLogo from '../img/sovrython/Interspacechat-logo.svg'
 
-const Header = styled.span``;
+const Header = styled.span``
+
+const PoweredBy = styled.div`
+  position: absolute;
+  bottom: 1rem;
+  width: 150px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  margin: 0 auto;
+  left: 0;
+  right: 0;
+`
+
+const Provider = styled.img`
+  transition: 1s cubic-bezier(0.2, 0.8, 0.2, 1);
+  filter: opacity(0.3);
+  cursor: pointer;
+  :hover {
+    filter: opacity(1);
+  }
+`
 
 const ImagemapContainer = styled.div`
- display: flex;
- height: 100%;
- width: 100%;
- margin: 0;
- margin: auto auto;
- align-items: center; 
- justify-content: center;
- text-align: center; 
- overflow-y: auto;
-`;
+  display: flex;
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  margin: auto auto;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  overflow-y: auto;
+`
 
 const ImagemapContainerMobile = styled.div`
   display: grid;
   place-items: center;
   min-height: 90vh;
-`;
+`
 
 const Headline = styled.h6`
   color: black;
@@ -37,22 +59,22 @@ const Headline = styled.h6`
 
   a {
     font-weight: 300;
-    color: ${(props) => props.theme.body};
-    background-color: ${(props) => props.theme.background};
+    color: ${props => props.theme.body};
+    background-color: ${props => props.theme.background};
     text-decoration: underline;
   }
 
   @media (max-width: 600px) {
     background-color: whitesmoke;
   }
-`;
+`
 
 const SpaceSelector = styled.div`
   .space-container {
     height: 100vh;
     display: grid;
   }
-`;
+`
 
 const SpaceInfo = styled.div`
   text-align: center;
@@ -62,9 +84,9 @@ const SpaceInfo = styled.div`
   font-size: 1rem;
   font-weight: 300;
   div {
-    color: ${(props) => props.theme.body};
+    color: ${props => props.theme.body};
   }
-`;
+`
 
 const Descripton = styled.div`
   z-index: 1;
@@ -89,41 +111,41 @@ const Descripton = styled.div`
       padding: 0.5rem;
     }
   }
-`;
+`
 
 const CurrentSpace = styled.span`
-  color: ${(props) => props.theme.highlight};
-`;
+  color: ${props => props.theme.highlight};
+`
 
 const StrongStyled = styled.strong`
   font-weight: 700;
-`;
+`
 
 const Triangle = styled.img`
   align-self: baseline;
-`;
+`
 
 const ShowSection = () => {
-  const [isHidden, toggleHidden] = useState(false);
+  const [isHidden, toggleHidden] = useState(false)
   const onClick = () => {
-    isHidden ? toggleHidden(false) : toggleHidden(true);
-  };
+    isHidden ? toggleHidden(false) : toggleHidden(true)
+  }
 
   const ButtonContainer = styled.div`
     position: absolute;
-    background-color: ${(props) => props.theme.background};
+    background-color: ${props => props.theme.background};
     opacity: 0.95;
     width: 100%;
     height: 100%;
     display: grid;
     justify-self: center;
     justify-content: end;
-  `;
+  `
   const CloseButton = styled.button`
     background: unset;
-    border: 1px solid ${(props) => props.theme.highlight};
+    border: 1px solid ${props => props.theme.highlight};
     border-radius: 100px;
-    color: ${(props) => props.theme.highlight};
+    color: ${props => props.theme.highlight};
     font-size: 2rem;
     font-weight: 100;
     padding: 1rem;
@@ -132,50 +154,56 @@ const ShowSection = () => {
     & :focus {
       filter: invert(100%);
     }
-  `;
+  `
   return (
     <div>
       {isHidden ? null : (
         <ButtonContainer>
           {isHidden ? null : <Element />}
           <CloseButton onClick={onClick}>
-            {isHidden ? "" : "Close this message"}
+            {isHidden ? '' : 'Close this message'}
           </CloseButton>
         </ButtonContainer>
       )}
     </div>
-  );
-};
+  )
+}
 const Element = () => (
   <Descripton>
     <p>
-      This website is optimized for <StrongStyled>desktop</StrongStyled>.{" "}
+      This website is optimized for <StrongStyled>desktop</StrongStyled>.{' '}
     </p>
     <p>To join the video-chat on your mobile, download the Jitsi Mobile App.</p>
     <p>Sovrython livestream hosted by Parallele.at:</p>
-    <a href="https://www.youtube.com/channel/UCsF67FGXtv4lplQyQSPizbQ">
+    <a href='https://www.youtube.com/channel/UCsF67FGXtv4lplQyQSPizbQ'>
       Parallele Polis Youtube channel
     </a>
   </Descripton>
-);
+)
 
 const Space = () => {
-  const { currentFloatingSpaces } = useContext(FloatingSpaceContext);
+  const { currentFloatingSpaces } = useContext(FloatingSpaceContext)
 
-  const space = currentFloatingSpaces;
+  const space = currentFloatingSpaces
 
-  let displayedJoinedSpaces;
+  let displayedJoinedSpaces
   if (space.length > 0) {
     if (space.length > 2) {
-      let nameCount = space.length;
+      let nameCount = space.length
       displayedJoinedSpaces =
-        space.slice(0, nameCount - 2).join(", ") +
-        ", " +
-        space.slice(nameCount - 2, nameCount).join(" & ");
+        space.slice(0, nameCount - 2).join(', ') +
+        ', ' +
+        space.slice(nameCount - 2, nameCount).join(' & ')
     } else {
-      displayedJoinedSpaces = space.join(" & ");
+      displayedJoinedSpaces = space.join(' & ')
     }
   }
+
+  const openInNewTab = url => {
+    let win = window.open(url, '_blank')
+    win.focus()
+  }
+
   {
     /**
   useEffect(() => {
@@ -191,10 +219,35 @@ const Space = () => {
 
   return (
     <SpaceSelector>
-      <BrowserView viewClassName="space-container">
+      <BrowserView viewClassName='space-container'>
         <ImagemapContainer>
           <SvgImagemap />
         </ImagemapContainer>
+        <PoweredBy>
+          <Provider
+            src={ParalleleLogo}
+            width='40px'
+            height='40px'
+            alt='Parallele Polis'
+            onClick={() => openInNewTab('https://twitter.com/parallelepolis/')}
+          />
+          <Provider
+            src={GitcoinLogo}
+            width='40px'
+            height='40px'
+            alt='Gitcoin'
+            onClick={() => openInNewTab('https://gitcoin.co/')}
+          />
+          <Provider
+            src={InterspaceLogo}
+            width='40px'
+            height='40px'
+            alt='interspace.chat'
+            onClick={() =>
+              openInNewTab('https://gitcoin.co/grants/516/interspacechat')
+            }
+          />
+        </PoweredBy>
         {/**
          <Header>
           <SpaceInfo>
@@ -220,7 +273,7 @@ const Space = () => {
         </ImagemapContainerMobile>
       </MobileView>
     </SpaceSelector>
-  );
-};
+  )
+}
 
-export default Space;
+export default Space
