f05de4c693333e9b2d75ddadf27ab7df6560437a
geleeroyale <krassvs@gmail.com>
2021-06-29 01:02:29 +0200

    add nft auction instance

---
+++ src/components/external-sites/OpenseaInstance.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function OpenseaInstance () {
+  return (
+    <iframe
+      src='https://opensea.io/assets?embed=true'
+      width='100%'
+      height='100%'
+      frameborder='0'
+      allowfullscreen
+    ></iframe>
+  )
+}
+
+export default OpenseaInstance
