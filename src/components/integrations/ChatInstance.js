1f2353f05da8a5e1e61e937b7fb957e3631f3a45
geleeroyale <krassvs@gmail.com>
2021-07-05 14:28:16 +0200

    replace widgetbot with status chat

--- src/components/integrations/ChatInstance.js
+++ src/components/integrations/ChatInstance.js
@@ -8,39 +8,24 @@ function ChatInstance () {
   const space = currentFloatingSpaces
 
   useEffect(() => {
-    if (space.indexOf('main-room') > -1) {
-      setDiscordRoom('692423172020240444')
-    } else if (space.indexOf('lobby') > -1) {
-      setDiscordRoom('692423341046366208')
-    } else if (space.indexOf('mentor-ring') > -1) {
-      setDiscordRoom('692423431777681488')
-    } else if (space.indexOf('claim poap token') > -1) {
-      setDiscordRoom('693135339249991701')
-    } else {
-      setDiscordRoom('692409996083855501')
+    {
+      window.StatusWidget(
+        'eth-cc',
+        document.getElementById('status-chat-widget')
+      )
     }
-    return console.log(discordRoom)
-  }, [space, discordRoom])
+  }, [])
 
   return (
     <div
       style={{
-        backgroundColor: '#1D1D1D',
-        color: '#fff',
         width: '100%',
         height: '95%',
         position: 'relative',
-        overflowY: 'auto'
+        overflow: 'hidden'
       }}
-    >
-      <WidgetBot
-        server='729675474665603133'
-        channel='823383734664691712'
-        shard='https://emerald.widgetbot.io'
-        width='100%'
-        height='100%'
-      />
-    </div>
+      id='status-chat-widget'
+    ></div>
   )
 }
 
